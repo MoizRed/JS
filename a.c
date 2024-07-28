3fdb57ad9e8e80fde54822b0148d11d4f516ade9
MoizRed <abdoureduniv2000@gmail.com>
2024-07-28 19:49:14 +0100

    TRENARY OPERATOR DONE!

---
+++ a.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include <string.h>
+
+int main(){
+
+
+    for(int i = 0; i <81 ; i++){
+
+    printf("%d",i);
+    printf("\n");
+
+
+    }
+return 0;
+}
---
+++ index.js
@@ -0,0 +1,100 @@
+/* 
+1 intro DONE!
+2 VARs DONE!
+3 ARITHMETICS DONE!
+4 USER INPUT DONE!
+5 TYPE CONVERSION DONE!
+6 CONSTANTS DONE!
+7 COUNTER PROJECT DONE! EASY.
+8 MATH IS DONE!
+9 RANDOM NUMBER GENERATOR DONE! EASY
+10 IF STATEMENT  + SWITCH  DONE!
+11 
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+
+*/
+
+let text = document.getElementById("count")
+let mycheckbox = document.getElementById("mycheckbox")
+
+let button = document.getElementById("mybutton")
+
+button.onclick = function(){
+
+    console.log(mycheckbox) 
+
+    mycheckbox.checked? text.textContent = "YES okey bro lol" : text.textContent = "NO , NOT OKEY BRO"
+
+    
+
+
+}
+
