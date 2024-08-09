46e33c7dbcbb55818a60699ab46eb568b4059618
MoizRed <abdoureduniv2000@gmail.com>
2024-08-09 17:27:20 +0100

    CLOSURE IS EASY JUST TO HAVE PRIVATE VARIABLES

--- index.js
+++ index.js
@@ -59,8 +59,8 @@
 
 LEFT(IMPORTANT) : 
 
-52 : CLOSURE (repeat it again)
-61 : ERROR HANDLING
+52 : CLOSURE (repeat it again) DONE!
+61 : ERROR HANDLING 
 64 : ELEMENT SELECTORS
 77 : JSON
 78 : COOKIES
@@ -93,38 +93,63 @@ PROJECTS(JS) :
 
 
 
-function makegame(){
 
-let score = 0
+// Error = An Object that is created to represent a problem that occurs
+
+//              Occur often with user input or establishing a connection
 
 
-function increaseScore(points){
 
-    score += points
-    console.log(`score : ${score}`)
+// try { } = Encloses code that might potentially cause an error
+
+// catch { } = Catch and handle any thrown Errors from try { }
+
+// finally { } = (optional) Always executes. Used mostly for clean up
+
+//                    ex. close files, close connections, release resources
+
+
+
+try{
+
+    const dividend = Number(window.prompt("Enter a dividend: "));
+
+    const divisor = Number(window.prompt("Enter a divisor: "));
+
+    
+
+    if(divisor == 0){
+
+        throw new Error("You can't divide by zero!");
+
+    }
+
+    if(isNaN(dividend) || isNaN(divisor)){
+
+        throw new Error("Values must be a number");
+
+    }
+
+
+
+    const result = dividend / divisor;
+
+    console.log(result);
+
+}
+
+catch(error){
+
+    console.error(error);
+
+}
+
+finally{
+
+    console.log("This always executes");
+
 }
 
 
 
-function decreaseScore(points){
-
-    score += points
-    console.log(`score : ${score}`)
-}
-
-function getscore(){
-
-    return score
-
-}
-
-return {increaseScore , decreaseScore , getscore}
-}
-
-const game = makegame()
-
-game.increaseScore(33)
-game.increaseScore(33)
-game.increaseScore(33)
-game.increaseScore(33)
-console.log("score : " + game.getscore())
+console.log("You have reached the end!"); 
